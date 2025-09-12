d8e1344feee7c2b28b8dc9ca8e631a23812554c9
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-09-12 22:23:06 +0200

    Devcontainer-oppsett med docker-compose og Dockerfile

--- server/app/routes.php
+++ server/app/routes.php
@@ -16,7 +16,7 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write('Hello Ylva!');
         return $response;
     });
 
