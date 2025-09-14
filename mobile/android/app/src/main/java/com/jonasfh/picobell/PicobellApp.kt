c2571b9e798521f3e94d5c085f548b055cb20ee3
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-09-14 02:07:47 +0200

    Minimal project activity running, printing FCM reg token

---
+++ mobile/android/app/src/main/java/com/jonasfh/picobell/PicobellApp.kt
@@ -0,0 +1,13 @@
+package com.jonasfh.picobell
+
+import android.app.Application
+import androidx.emoji.text.EmojiCompat
+import androidx.emoji.bundled.BundledEmojiCompatConfig
+
+class PicobellApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val config = BundledEmojiCompatConfig(this)
+        EmojiCompat.init(config)
+    }
+}
