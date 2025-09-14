5f754106d919caaab471da6551856a9e5cf2ade7
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-09-14 02:07:47 +0200

    Minimal project activity running, printing FCM reg token

--- mobile/android/app/src/main/java/com/jonasfh/picobell/MainActivity.kt
+++ mobile/android/app/src/main/java/com/jonasfh/picobell/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.jonasfh.picobell
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jonasfh.picobell.ui.theme.PicoBellTheme
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PicoBellTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("FCM", "Fetching FCM token failed", task.exception)
+                    return@addOnCompleteListener
                 }
+                val token = task.result
+                Log.d("FCM", "FCM registration token: $token")
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PicoBellTheme {
-        Greeting("Android")
     }
 }
---
+++ mobile/android/app/src/main/java/com/jonasfh/picobell/MyFirebaseMessagingService.kt
@@ -0,0 +1,15 @@
+package com.jonasfh.picobell
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d("FCM", "Message received: ${message.data}")
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d("FCM", "New token: $token")
+    }
+}
