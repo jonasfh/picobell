44b76faa39fdc87de94ab838bdff2f4bf09f3af1
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-09-15 00:09:31 +0200

    Support for notifications

--- mobile/android/app/src/main/java/com/jonasfh/picobelltaco/MainActivity.kt
+++ mobile/android/app/src/main/java/com/jonasfh/picobelltaco/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.jonasfh.picobelltaco
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    1001
+                )
+            }
+        }
 
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener { task ->
