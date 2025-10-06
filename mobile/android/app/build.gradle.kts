0f9d913f93e2df21a3c62b59b7bd6f6a5ac2795b
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-10-06 21:14:34 +0200

    Test-bibliotek

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -37,7 +37,12 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.emoji:emoji-bundled:1.1.0")
     implementation("com.google.firebase:firebase-messaging:23.3.1")
+    // Google / HTTP / Coroutines
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    // --- TESTING ---
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
