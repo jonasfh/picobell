673c8ee053cfc113f1b2f6958684825119ba821f
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-12-06 17:32:39 +0100

    (#27) Open app when clicking notification

--- mobile/android/app/src/main/java/com/jonasfh/picobelltaco/auth/PicobellFirebaseMessagingService.kt
+++ mobile/android/app/src/main/java/com/jonasfh/picobelltaco/auth/PicobellFirebaseMessagingService.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.R
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.util.Log
@@ -13,6 +15,7 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.edit
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.jonasfh.picobelltaco.MainActivity
 
 class PicobellFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -23,7 +26,7 @@ class PicobellFirebaseMessagingService : FirebaseMessagingService() {
 
 
         // Notification payload
-        showNotification("Picobell", "Noen ringer på ${remoteMessage.data["address"]}")
+        showNotification("Ding dong", "Noen ringer på ${remoteMessage.data["address"]}")
 
         // Save data payload indexed by apartment id
         val aptId = remoteMessage.data["apartment_id"]?.toIntOrNull()
@@ -47,6 +50,20 @@ class PicobellFirebaseMessagingService : FirebaseMessagingService() {
     @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
     private fun showNotification(title: String?, message: String?) {
         val channelId = "picobell_channel"
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("from_notification", true)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -64,6 +81,8 @@ class PicobellFirebaseMessagingService : FirebaseMessagingService() {
             .setContentText(message ?: "Noen ringer på!")
             .setSmallIcon(R.drawable.ic_dialog_info)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)   // <<< åpner appen
+            .setAutoCancel(true)
             .build()
 
         NotificationManagerCompat.from(this).notify(1, notification)
