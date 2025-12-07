275062446fcd239dbfc1a7e7300bb13a2033f666
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2025-12-07 20:19:22 +0000

    (#34) Update controller with api key

--- server/src/Controllers/ApartmentController.php
+++ server/src/Controllers/ApartmentController.php
@@ -10,19 +10,36 @@ class ApartmentController {
     public function __construct(Medoo $db) { $this->db = $db; }
 
     public function list(Request $req, Response $res): Response {
-        $apartments = $this->db->select("apartments", "*");
+        $apartments = $this->db->select("apartments", [
+            "id",
+            "address",
+            "pico_serial",
+            "created_at",
+            "modified_at"
+        ]);
+
         $res->getBody()->write(json_encode($apartments));
         return $res->withHeader('Content-Type', 'application/json');
     }
 
     public function create(Request $req, Response $res): Response {
         $data = $req->getParsedBody();
+
+        $apiKey = bin2hex(random_bytes(32));
+
         $this->db->insert("apartments", [
             "address" => $data['address'],
+            "pico_serial" => $data['pico_serial'] ?? null,
+            "api_key" => $apiKey,
             "created_at" => date("Y-m-d H:i:s"),
             "modified_at" => date("Y-m-d H:i:s")
         ]);
-        $res->getBody()->write(json_encode(["id" => $this->db->id()]));
+
+        $res->getBody()->write(json_encode([
+            "id" => $this->db->id(),
+            "api_key" => $apiKey
+        ]));
+
         return $res->withHeader('Content-Type', 'application/json');
     }
 }
